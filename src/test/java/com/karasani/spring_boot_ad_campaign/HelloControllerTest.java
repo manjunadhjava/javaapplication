1f39215c394629b75b2401ccd40995dca6abce1c
Manjunadh <Manjunadhr02@gmail.com>
2017-07-18 21:11:29 -0400

    second commit

--- src/test/java/com/karasani/spring_boot_ad_campaign/HelloControllerTest.java
+++ src/test/java/com/karasani/spring_boot_ad_campaign/HelloControllerTest.java
@@ -17,22 +17,21 @@ import static org.hamcrest.CoreMatchers.is;
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = App.class)
 @WebAppConfiguration
-@IntegrationTest({"server.port:0",
-        "spring.datasource.url:jdbc:h2:mem:spring-boot-ad-campaign;DB_CLOSE_ON_EXIT=FALSE"})
+@IntegrationTest({"server.port:0"})
 public class HelloControllerTest {
     @Value("${local.server.port}")
     int port;
 
-//    @Before
-//    public void setUp() throws Exception {
-//        RestAssured.port = port;
-//    }
-//
-//    @Test
-//    public void testHello() throws Exception {
-//        when().get("/").then()
-//                .body(is("Hello World!"));
-//    }
+    @Before
+    public void setUp() throws Exception {
+        RestAssured.port = port;
+    }
+
+    @Test
+    public void testHello() throws Exception {
+        when().get("/users/1").then()
+                .body("name", is("First user"));
+    }
 //
 //    @Test
 //    public void testCalc() throws Exception {
